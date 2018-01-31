c6b58d68ab64fbd17b4bd4dfe52930b9841296c4
Dan DeGreef <daniel_degreef@condenast.com>
2018-01-31 11:22:03 -0600

    add token and crowdsale

---
+++ contracts/UprightlyCrowdsale.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.18;
+
+import "../node_modules/zeppelin-solidity/contracts/crowdsale/FinalizableCrowdsale.sol";
+import "./UprightlyProtocolToken.sol";
+
+/**
+ * @title SampleCrowdsale
+ * @dev This is an example of a fully fledged crowdsale.
+ * The way to add new features to a base crowdsale is by multiple inheritance.
+ *
+ */
+contract UprightlyCrowdsale is FinalizableCrowdsale {
+  function createTokenContract() internal returns (MintableToken) {
+    return new UprightlyProtocolToken();
+  }
+
+  /**
+   * After the crowdsale has ended, this will be called.
+   *
+   * Here we mint tokens for the Team to meet the allocated
+   * percentages as described in the whitepaper.
+   *
+   *
+   */
+  function finalization() internal {
+    super.finalization();
+
+    uint256 totalTokensFromCrowdsale = token.totalSupply(); // 60%
+
+    uint256 teamTokens = totalTokensFromCrowdsale * 28 / 60; // 28%
+    uint256 bountyTokens = totalTokensFromCrowdsale * 2 / 60; // 2%
+    uint256 futureIcoTokens = totalTokensFromCrowdsale * 10 / 60; // 10%
+
+    token.mint(owner, teamTokens);
+    token.mint(owner, bountyTokens);
+    token.mint(owner, futureIcoTokens);
+
+    // crowdsale is over at this point. No more tokens will be created.
+    token.finishMinting();
+
+  }
+}
